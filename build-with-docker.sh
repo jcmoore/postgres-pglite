4011b357e447cd944471eb72e4b8445dff71f9d4
Justin C. Moore <jcmoore@users.noreply.github.com>
2026-02-20 10:20:50 -0600

    Modify build script to make initial use more seamless.

--- build-with-docker.sh
+++ build-with-docker.sh
@@ -2,12 +2,14 @@
 # allows the DWARF info (when building in DEBUG) to contain the correct file paths
 DOCKER_WORKSPACE=$(pwd)
 
+mkdir -p "${DOCKER_WORKSPACE}/dist"
+
 docker run $@ \
   --rm \
   -e DEBUG=${DEBUG:-false} \
   --workdir=${DOCKER_WORKSPACE} \
-  -v .:${DOCKER_WORKSPACE}:rw \
-  -v ./dist:/install/pglite:rw \
+  -v ${DOCKER_WORKSPACE}:${DOCKER_WORKSPACE}:rw \
+  -v ${DOCKER_WORKSPACE}/dist:/install/pglite:rw \
   electricsql/pglite-builder:3.1.74_4 \
   ./build-pglite.sh
   
